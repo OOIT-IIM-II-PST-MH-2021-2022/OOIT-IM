59d50f4a2ef07b28d972be73e72375b895a131bd
NatalijaGajic <nat.gaj98@gmail.com>
2021-11-04 17:03:39 +0100

    URIS Vezbe 3 - Klasa Circle

---
+++ IM-Projekat/src/geometry/Circle.java
@@ -0,0 +1,45 @@
+package geometry;
+
+public class Circle {
+
+	private Point center;
+	private int radius;
+	private boolean selected;
+	
+	public Circle() {
+		
+	}
+	
+	public Circle(Point center, int radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+
+	public Point getCenter() {
+		return center;
+	}
+
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+	public double area() {
+		return radius * radius * Math.PI;
+	}
+}
