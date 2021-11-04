aecf664f82f7b9a3dc5b8233b60d7ee78bd596af
Vladimir <Vladimir@DESKTOP-BAQNI43>
2021-11-04 10:26:38 +0100

    Vezbe 3

--- IM-Projekat/src/geometry/Test.java
+++ IM-Projekat/src/geometry/Test.java
@@ -8,6 +8,28 @@ public class Test {
 		Point p2 = new Point();
 		System.out.println(p1.getX());
 		System.out.println(p2.getX());
+		
+		//Vezbe 3
+		p2.setX(21);
+		p2.setY(30);
+		p1.setY(22);
+		
+		System.out.println(p1.distance(p2));
+		Line l1 = new Line();
+		Line l2 = new Line();
+		l1.setStartPoint(p2);
+		l1.setEndPoint(p1);
+		System.out.println(l1.length());
+		
+		Rectangle r1 = new Rectangle();
+		r1.setUpperLeft(p1);
+		r1.setHeight(20);
+		r1.setWidth(20);
+		System.out.println(r1.area());
+		System.out.println(r1.circumference());
+		
+		l1.getEndPoint().setY((int)r1.getUpperLeft().distance(r1.getUpperLeft())+r1.area());
+		System.out.println("Vrednost y koordinate krajnje tacke linije l1 je: " + l1.getEndPoint().getY());
 	}
 
 }
